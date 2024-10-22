53303cdd45eb33f2f2a7ed209dea922ff2a14a29
Krengsak <kes1164109051022@gmail.com>
2024-10-22 12:36:36 +0700

    Update T_Controller.js

--- srbc/controller/T_Controller.js
+++ srbc/controller/T_Controller.js
@@ -45,7 +45,7 @@ export const getLabTest = async (req,res) =>{
 
 
         const LabTest = {
-            LabTest_ID
+            LabTest_ID,
             TestName,
             TestType,
             TestPrice
